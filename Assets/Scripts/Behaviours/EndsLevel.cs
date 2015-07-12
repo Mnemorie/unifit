7beba5d2b9670622b08ccf6493e29082edb023d7
simon <simon@simon.com>
2015-07-12 14:04:03 -0400

    More Levels and End With core only

--- Assets/Scripts/Behaviours/EndsLevel.cs
+++ Assets/Scripts/Behaviours/EndsLevel.cs
@@ -3,6 +3,16 @@ using System.Collections;
 
 public class EndsLevel : MonoBehaviour {
 	public GameController gameController;
+	public float TimeRequiredToWin;
+	public float RotationSpeed ;
+
+	private GameObject intruder;
+	private float IntruderEnteredAtTime;
+
+	public float TimeSpentInside;
+
+
+
 	// Use this for initialization
 	void Start () {
 		gameController = GameObject.Find ("GameController").GetComponent<GameController>();
@@ -10,17 +20,52 @@ public class EndsLevel : MonoBehaviour {
 
 	void OnTriggerEnter(Collider collider){
 		if (collider.gameObject.GetComponent<CanEndLevel>() != null) {
-			EndLevel();
+			this.intruder = collider.gameObject;
+			IntruderEnteredAtTime =Time.time;
 		}
 	}
+
+	void OnTriggerLeave(){
+		this.intruder = null;
+		this.TimeSpentInside = 0f;
+		ResetDisplay ();
+	}
+
+	void OnTriggerExit(){
+		this.intruder = null;
+		this.TimeSpentInside = 0f;
+		ResetDisplay ();
+	}
+
+	void OnTriggerStay(Collider collider){
+		if (collider.gameObject != this.intruder) {
+			return;
+		}
+
+		ChangeDisplayWhenInside ();
+
+		this.TimeSpentInside = Time.time - IntruderEnteredAtTime;
+
+		if (this.TimeSpentInside > TimeRequiredToWin)
+			EndLevel ();
+	}
+
+
+	void ChangeDisplayWhenInside(){
+		this.transform.RotateAround (this.transform.position, Vector3.left, RotationSpeed * Time.deltaTime);
+	}
+	void ResetDisplay(){
+		this.transform.localRotation = Quaternion.identity;
+	}
+
 
 	void EndLevel(){
 		Debug.Log ("Level Ended");
 		gameController.EndLevel ();
 	}
-
 	// Update is called once per frame
 	void Update () {
-	
+
+
 	}
 }
