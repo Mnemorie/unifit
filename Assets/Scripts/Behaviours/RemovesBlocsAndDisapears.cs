01cea0c484049ec0a66469c0f4f1918738120c16
simon <simon@simon.com>
2015-07-12 10:24:57 -0400

    Added GameLimit to Cage

---
+++ Assets/Scripts/Behaviours/RemovesBlocsAndDisapears.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class RemovesBlocsAndDisapears : MonoBehaviour {
+
+	public List<GameObject> ToDestroy;
+
+	// Use this for initialization
+	void Start () {
+		if (ToDestroy == null)
+			return; 	
+		foreach (GameObject go in ToDestroy) {
+			Destroy(go);
+			Destroy(this.gameObject);
+		}
+			
+	}
+
+
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
