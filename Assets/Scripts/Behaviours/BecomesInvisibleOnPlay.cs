42bcdc6432409f47b45ac02bca316527ea3df886
simon <simon@simon.com>
2015-07-12 10:08:02 -0400

    More Levels and blocks

---
+++ Assets/Scripts/Behaviours/BecomesInvisibleOnPlay.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BecomesInvisibleOnPlay : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		this.GetComponent<MeshRenderer> ().enabled = false;
+
+
+	}
+	
+
+}
---
+++ Assets/Scripts/Behaviours/RemovesBlocAndDisapears.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RemovesBlocAndDisapears : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+
+	void OnTriggerEnter(Collider collider){
+		Debug.Log ("RRRrrr");
+	}
+
+	void OnTriggerStay(Collider collider){
+		Debug.Log ("Destroying: " + collider);
+		Destroy (collider.gameObject);
+		Destroy (this.gameObject);	
+	}
+
+
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
