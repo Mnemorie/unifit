e5db3413053705e7f82ab6fe87048b59cd390ceb
Luc Tremblay <luc@tons-of-slack.net>
2015-07-20 15:44:34 -0400

    face of god looking up

---
+++ Assets/AlignUpwards.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+public class AlignUpwards : MonoBehaviour
+{
+    Vector3 axis;
+
+	void Start () 
+    {
+        axis = Vector3.left;
+	}
+
+    public float DebugAngle;
+
+	void Update () 
+    {
+        float parentAngleToWorldUp = Vector3.Angle(transform.parent.forward, Vector3.up);
+        if (Vector3.Cross(transform.parent.forward, Vector3.up).x < 0)
+        {
+            parentAngleToWorldUp = -parentAngleToWorldUp;
+        }
+        transform.localRotation = Quaternion.AngleAxis(parentAngleToWorldUp, axis);
+	}
+}
